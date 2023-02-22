09ea35ac1cdee572a2909978738b5fd7040cd39c
Andy Park <atpark333@gmail.com>
2023-02-22 23:09:38 +0900

    Add scaffolding for Merge two sorted lists
    https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/com/leetcode/MergeTwoSortedLists/Solution.java
@@ -0,0 +1,7 @@
+package com.leetcode.MergeTwoSortedLists;
+
+public class Solution {
+    public ListNode mergeTwoSortedLists(ListNode list1, ListNode list2) {
+        return new ListNode();
+    }
+}
